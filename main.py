628c2bc352933b6070c064d14f79c0729788eb0b
artemosypovPro <papir.os124@gmail.com>
2024-11-27 18:20:43 +0200

    expenses.py

--- main.py
+++ main.py
@@ -1,2 +1,6 @@
-print("Hello World")
-print("Hello Git")
+expenses = {"hotel": 150, "breakfast": 30 , "taxi": 15 , "lunch": 20}
+
+file_name = "expenses.txt"
+with open(fine_name, "w") as fh: 
+    for key , value in expenses.items():
+        fh.write(f"{key}|{value}\n")
